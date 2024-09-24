dccc94d71b43aed259e0cc712971e58c5edde563
Maneesh Tewani <mtewani@google.com>
2024-09-24 19:51:23 +0100

    Updated main.dart

--- dart_movie_app/lib/main.dart
+++ dart_movie_app/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 // TODO: Remove the following line when your SDK has been generated
-import './generated/movies.dart';
+// import './generated/movies.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
